d9b0b103b71ba2274b8c48b3721ff71eee97c9af
Maël Delem <mael.a.delem@gmail.com>
2024-10-22 06:32:57 +0200

    define contrasts, sum consecutive, and all set

--- R/define_contrasts.R
+++ R/define_contrasts.R
@@ -1,4 +1,26 @@
+#' Define contrasts inside a pipeline
+#' @description
+#' Short wrapper around the `contrasts` function to define contrasts inside a pipeline easily. The function takes a dataframe as input and returns a dataframe with updated contrasts, allowing to pipe it, which differs from the usual use of the `contrasts` function.
+#'
+#'
+#' @param df A dataframe.
+#' @param col A column name.
+#' @param contrast A matrix of contrasts.
+#'
+#' @return A dataframe with the contrasts defined.
+#' @export
+#'
+#' @examples
+#' df <- data.frame(group = factor(rep(c("A", "B", "C"), 3)))
+#'
+#' # The usual way
+#' contrasts(df$group) <- contr.sum(3)
+#'
+#' # the pipeline way
+#' df <- df |> define_contrasts(group, contr.sum(3))
+#'
+#' contrasts(df$group)
 define_contrasts <- function(df, col, contrast) {
-  contrasts(df[[col]]) <- contrast
+  contrasts(df[[ensym(col)]]) <- contrast
   return(df)
 }
---
+++ R/delemr-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import rlang
+#' @import dplyr
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
+#' @importFrom stats contrasts<-
+## usethis namespace: end
+NULL
